38523d9003dfcd51680423da8e6d2b9827fecffd
tiagochoucair <tiago.choucair@gmail.com>
2015-05-05 09:12:09 -0500

    Update DBConfig.js

--- APIs/DBConfig.js
+++ APIs/DBConfig.js
@@ -1,8 +1,8 @@
 var DBConfig = {
-        user: 'egaviria',
-        password: 'medellin2015+',
-        server: '172.17.27.185\\chc_bd', // You can use 'localhost\\instance' to connect to named instance
-        database: 'MaxTimeCHC',
+        user: '',
+        password: '+',
+        server: '', // You can use 'localhost\\instance' to connect to named instance
+        database: '',
 
         options: {
             encrypt: true // Use this if you're on Windows Azure
